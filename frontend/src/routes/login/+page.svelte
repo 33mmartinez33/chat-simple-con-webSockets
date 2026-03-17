ba9506edcfcaecf1f468dbbd2c56df703b201dd0
33mmartinez33 <33mmartinez33@gmail.com>
2026-03-17 11:54:28 +0100

    Created all routes and made UI of home, login and sign_in

---
+++ frontend/src/routes/login/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+    function login() {
+        // logica
+    }
+</script>
+
+<main>
+    <h1>¡Bienvenido!</h1>
+    <div>
+        <form action="">
+            <label for="username" id="primerLabel">Nombre de usuario</label>
+            <input type="text" id="username">
+
+            <label for="contraseña">Contraseña</label>
+            <input type="password" id="contraseña">
+
+            <button class="botonFormulario" onclick={login}>Iniciar Sesión</button>
+        </form>
+    </div>
+</main>
+
+<style>
+    h1{
+        font-size: 60px;
+        margin-top: 170px;
+    }
+    div{
+        margin-top: 40px;
+        width: 280px;
+        margin-left: auto;
+        margin-right: auto;
+    }
+</style>
---
+++ frontend/src/routes/sign_in/+page.svelte
@@ -0,0 +1,44 @@
+<script>
+    function registrarse() {
+        // Lógica
+    }
+</script>
+
+<main>
+    <h1>¡Únete a Nexus!</h1>
+    <div>
+        <label for="email">Email</label>
+        <input type="email" id="email">
+        
+        <label for="username">Nombre de usuario</label>
+        <input type="text" id= "username">
+
+        <label for="password">Contraseña</label>
+        <input type="password" id = "password">
+
+        <label for="birthdate">Fecha de nacimiento</label>
+        <input type="date" id = "birthdate">
+
+        <button class="botonFormulario" onclick={registrarse}>Registrarse</button>
+    </div>
+</main>
+
+<style>
+
+    h1{
+        margin-top: 110px;
+    }
+    div{
+        margin-top: 40px;
+        width: 280px;
+        margin-left: auto;
+        margin-right: auto;
+    }
+/* Invierte el color del icono del calendario de negro a blanco*/
+    input[type="date"]::-webkit-calendar-picker-indicator {
+    filter: invert(1);
+    cursor: pointer;
+    padding: 4px;
+    border-radius: 4px;
+    }
+</style>
